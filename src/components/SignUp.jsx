0bf0eaae6843deefac9f3b00f982b5277cc7a5b9
AvelyV <avely.voore@gmail.com>
2022-02-02 10:30:07 +1000

    implemented react-hook-form, not working properly

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,33 +1,28 @@
-import { useState } from 'react'
-import axios from 'axios'
-// import 'core-js/es6/promise';
-// import 'core-js/es6/set';
-// import 'core-js/es6/map';
-// import * as yup from 'yup';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import axios from 'axios';
 
 function Form() {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [file, setFile] = useState();
 
-  const [username, setUsername] = useState("")
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
-  const [file, setFile] = useState()
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
-  // let schema = yup.object().shape({
-  //   username: yup.string().required(),
-  //   password: yup.string().required(),
-  //   email: yup.string().email(),
-  // });
-
-  function handleSubmit(e) {
-    
-    e.preventDefault()
-    const url = 'http://localhost:3000/signup'
-    const formData = new FormData()
-    formData.append('file', file)
-    formData.append('fileName', file.name)
-    formData.append('username', username)
-    formData.append('email', email)
-    formData.append('password', password)
+  function onSubmit(e) {
+    e.preventDefault();
+    const url = 'http://localhost:3000/signup';
+    const formData = new FormData();
+    formData.append('file', file);
+    formData.append('fileName', file.name);
+    formData.append('username', username);
+    formData.append('email', email);
+    formData.append('password', password);
 
     // const config = {
     //   headers: {
@@ -41,48 +36,56 @@ function Form() {
   }
 
   return (
-
     <div className="App">
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <h1> File upload </h1>
         <div>
-            <input 
-                type="text"
-                // validationSchema={schema}
-                id="username"
-                name="username"
-                onChange={(e) => setUsername(e.target.value)}
-                value={username}
-                placeholder="Username"/>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            onChange={(e) => setUsername(e.target.value)}
+            value={username}
+            placeholder="Username"
+            // validation
+            {...register('username', { required: true, minLength: 2 })}
+          />
+          {errors.username && <p>Username must be at least 2 characters.</p>}
         </div>
 
         <div>
-            <input 
-                type="email"
-                // validationSchema={schema}
-                id="email"
-                name="username"
-                onChange={(e) => setEmail(e.target.value)}
-                value={email}
-                placeholder="Email"/>
+          <input
+            type="email"
+            id="email"
+            name="username"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+            placeholder="Email"
+            // validation
+            {...register('email', { required: true })}
+          />
+          {errors.email && <p>Invalid email</p>}
         </div>
 
         <div>
-            <input 
-                type="password"
-                // validationSchema={schema}
-                id="password"
-                name="password"
-                onChange={(e) => setPassword(e.target.value)}
-                value={password}
-                placeholder="Password"/>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+            placeholder="Password"
+            // validation
+            {...register('password', { required: true })}
+          />
+          {errors.password && <p>Password must be at least 6 characters</p>}
         </div>
 
         <div>
-            <input type="file" onChange={(e) => setFile(e.target.files[0])} />
+          <input type="file" onChange={(e) => setFile(e.target.files[0])} />
         </div>
 
-        <button type="submit" > Upload </button>
+        <button type="submit"> Upload </button>
       </form>
     </div>
   );
