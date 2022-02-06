3dc3c4027bc23933072c1035d05b67d99d1e5999
AvelyV <avely.voore@gmail.com>
2022-02-07 09:52:59 +1000

    Added some links to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,14 +9,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 export default function SearchAppBar() {
 
   return (
-<Navbar bg="light" expand="lg">
+<Navbar bg="light" expand="lg" fixed="top">
   <Container>
     <Navbar.Brand href="/">Explore</Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="me-auto">
-        <Nav.Link href="#home">Home</Nav.Link>
-        <Nav.Link href="#link">Link</Nav.Link>
+        <Nav.Link href="/">Home</Nav.Link>
+        <Nav.Link href="/about">About</Nav.Link>
         <NavDropdown title="Username" id="basic-nav-dropdown">
           <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
           <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
