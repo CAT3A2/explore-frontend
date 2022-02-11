57ff9108749bcabb86c0d34047e291220201c069
AvelyV <avely.voore@gmail.com>
2022-02-11 18:59:20 +1000

    fixed environmental variable

--- src/api.js
+++ src/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'http://localhost:5500/'
-    // baseURL: import.meta.env.API_URL
-    // baseURL: process.env.REACT_APP_API_URL
+    // baseURL: 'http://localhost:5500/'
+    // baseURL: import.meta.env.REACT_APP_API_URL
+    baseURL: process.env.REACT_APP_API_URL
 })
 
 export default api
