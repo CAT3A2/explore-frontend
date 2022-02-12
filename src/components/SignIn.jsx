446b24abbe940fda6f02032f761d0cc6474670e9
Shane Qi <shane_qi@hotmail.com>
2022-02-13 07:24:06 +1000

    fix auth error message and fix login path

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useState } from 'react';
 import { useForm } from 'react-hook-form';
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom';
 // import axios from 'axios';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
@@ -8,11 +8,10 @@ import Container from '@mui/material/Container';
 import ExploreContext from '../ExploreContext';
 import Alert from 'react-bootstrap/Alert';
 import { useNavigate } from 'react-router-dom';
-import { useCookies } from "react-cookie";
-import api from './../api'
+import { useCookies } from 'react-cookie';
+import api from './../api';
 
 export default function SignIn() {
-
   let navigate = useNavigate();
   const {
     register,
@@ -21,42 +20,49 @@ export default function SignIn() {
   } = useForm();
   const { dispatch } = useContext(ExploreContext);
   const [serverError, setServerError] = useState();
-  const [cookies, setCookie] = useCookies(['tokenCookie'])
-
+  const [cookies, setCookie] = useCookies(['tokenCookie']);
+  const [authError, setAuthError] = useState('');
 
   function onSubmit(data) {
     // const url = 'http://localhost:5500/auth/login';
 
-    api.post('auth/login', data).then((response) => {
+    console.log(data);
 
-      dispatch({
-        // store the user information that was returned with the response in global store
-        type: 'setCurrentUser',
-        data: response.data.user,
+    api
+      .post('http://localhost:5500/auth/login', data)
+      .then((response) => {
+        dispatch({
+          // store the user information that was returned with the response in global store
+          type: 'setCurrentUser',
+          data: response.data.user,
+        });
+        // store accessToken extracted from response in a cookie
+        setCookie('tokenCookie', response.data.accessToken);
+        console.log(cookies.tokenCookie);
+
+        navigate('/');
+      })
+      .catch(function (error) {
+        if (error.message === 'Request failed with status code 404') {
+          setAuthError('Your username or password are incorrect, please try again');
+        }
+        console.log(error);
+        console.log(error.message);
+        if (error.response) {
+          setServerError(error.response.data);
+        } else if (error.request) {
+          console.log(error.request);
+        } else {
+          console.log('Error', error.message);
+        }
       });
-      // store accessToken extracted from response in a cookie
-      setCookie('tokenCookie', response.data.accessToken)
-      console.log(cookies.tokenCookie)
-      
-
-      navigate('/');
-    })
-    .catch(function (error) {
-      if (error.response) {
-        setServerError(error.response.data);
-      } else if (error.request) {
-        console.log(error.request);
-      } else {
-        console.log('Error', error.message);
-      }
-    });
   }
 
   return (
     <Container maxWidth="sm">
       <Form onSubmit={handleSubmit(onSubmit)}>
-      <h1> Sign In </h1>
-        {serverError && <Alert variant="danger"> {serverError} </Alert>}
+        <h1> Sign In </h1>
+        {authError && <Alert variant="danger"> {authError} </Alert>}
         <Form.Group>
           <Form.Label>Username</Form.Label>
           <Form.Control
